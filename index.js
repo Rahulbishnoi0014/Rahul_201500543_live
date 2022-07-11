ba74f7324e8be718677f77382aad7cf89eb31bf8
Rahul Bishnoi <97785343+Rahulbishnoi0014@users.noreply.github.com>
2022-07-11 19:26:16 +0530

    final assignment uploaded

---
+++ index.js
@@ -0,0 +1,15 @@
+const Name = "Rahul Bishnoi"
+const college = "GLA Mathura"
+const Roll_no = "201500543"
+
+const date = new Date();
+
+
+const tdate = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+
+
+
+document.getElementById("studentName").innerHTML = Name;
+document.getElementById("rollno.").innerHTML = Roll_no;
+document.getElementById("college").innerHTML = college;
+document.getElementById("date").innerHTML = tdate;
